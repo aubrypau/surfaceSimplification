1b141f17e9a9c27a1fe9b33ba55fd4408fe26d1c
aubrypau <paul.aubry.ent@gmail.com>
2023-04-24 14:22:22 +0200

    enlève les trucs nuls

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import polyscope as ps
 import numpy as	np
 from wavefront import *
 
-obj = load_obj( 'feline_half.obj')
 ps.init()
+obj = load_obj( 'feline_half.obj')
 ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_faces() )
 
 # `verts` is a Nx3 numpy array of vertex positions
@@ -11,4 +11,9 @@ ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_face
 # verts=np.array([[1.,0.,0.],[0.,1.,0.],[-1.,0.,0.],[0.,-1.,0.],[0.,0.,1.]])
 # faces=[[0,1,2,3],[1,0,4],[2,1,4],[3,2,4],[0,3,4]]
 #ps.register_surface_mesh("my mesh", verts, faces )
+# bdry  = obj.numpy_boundary_edges()
+# ps_net= ps.register_curve_network("boundary", obj.only_coordinates(), bdry )
+L = obj.ordered_boundary()
+print(L)
+
 ps.show()
