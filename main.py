917dc71cb47e99570946e19bc031d21e227a033a
AlexOS <82172155+alexandredesbos@users.noreply.github.com>
2023-05-05 11:02:20 +0200

    etape 2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import polyscope as ps
 import numpy as	np
 from wavefront import *
+import math
 
 ps.init()
-obj = load_obj( 'feline_half.obj')
-ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_faces() )
+obj = load_obj( 'test_cube.obj')
+
 
 # `verts` is a Nx3 numpy array of vertex positions
 # `faces` is a Fx3 array of indices, or a nested list
@@ -15,6 +16,53 @@ ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_face
 # ps_net= ps.register_curve_network("boundary", obj.only_coordinates(), bdry )
 
 
+############ Etape 2 ############
+
+t = 0  # threshold
+valid_pairs = []  # list of valid pairs
+
+
+# if v1 and v2 are connected by an edge return true
+def is_edge(v1, v2):
+    # Check if vertices share the same x, y, or z coordinate value
+    if v1[0] == v2[0] and v1[1] == v2[1]:
+        # Check if distance between vertices is equal to edge length
+        if math.isclose(abs(v1[2] - v2[2]), 2):
+            return True
+    elif v1[1] == v2[1] and v1[2] == v2[2]:
+        if math.isclose(abs(v1[0] - v2[0]), 2):
+            return True
+    elif v1[0] == v2[0] and v1[2] == v2[2]:
+        if math.isclose(abs(v1[1] - v2[1]), 2):
+            return True
+    return False
+
+def all_valid_pairs(obj, t):
+    # compare each pair of vertices and chek if they are valid
+    for v1 in obj.vertices:
+        for v2 in obj.vertices:
+            if v1 != v2 and is_edge(v1, v2) or np.linalg.norm(np.subtract(v1, v2)) < t:
+                valid_pairs.append((v1, v2))
+
+all_valid_pairs(obj, t)
+print("valid pairs")
+print(valid_pairs)
+
+
+
+####################################
+
+
+# remove the vertex v1 from the mesh
+def remove_vertex(obj, vertex_index):
+    # Remove the vertex from the vertex list
+    obj.vertices = np.delete(obj.vertices, vertex_index, 0) 
+
+
+# supprime une face
+# remove_vertex(obj, 0)
+    
+ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_faces() )
 ps.show()
 
 L = obj.ordered_boundary()
