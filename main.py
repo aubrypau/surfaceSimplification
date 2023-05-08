fe269f670190313568ea7f8c2c797eafc430b8c9
BaptisteGriva <75640057+ThxZ3US@users.noreply.github.com>
2023-05-08 11:40:44 +0200

    étape 1 pas finie

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 import polyscope as ps
-import numpy as	np
+import numpy as np
 from wavefront import *
 import math
 
 ps.init()
-obj = load_obj( 'test_cube.obj')
+obj = load_obj("test_cube.obj")
 
 
 # `verts` is a Nx3 numpy array of vertex positions
 # `faces` is a Fx3 array of indices, or a nested list
 # verts=np.array([[1.,0.,0.],[0.,1.,0.],[-1.,0.,0.],[0.,-1.,0.],[0.,0.,1.]])
 # faces=[[0,1,2,3],[1,0,4],[2,1,4],[3,2,4],[0,3,4]]
-#ps.register_surface_mesh("my mesh", verts, faces )
+# ps.register_surface_mesh("my mesh", verts, faces )
 # bdry  = obj.numpy_boundary_edges()
 # ps_net= ps.register_curve_network("boundary", obj.only_coordinates(), bdry )
+
+
+############ Etape 1 ############
+# Step 1: Compute the Q matrices for all the initial vertices.
+
+
+def getAllEdges(v1):
+    tab = []
+    for v2 in obj.vertices:
+        if v1 != v2:
+            if is_edge(v1, v2):
+                tab.append((v2))
+    return tab
+
+
+def getAllFaces(v1):
+    tab = []
+    tabv2 = getAllEdges(v1)
+
 
 ############ Calculer a b c d pour un plan ############
 
 """
 Soit un tirangle PRQ, avec P le sommet, calcul les vecteurs PR et PQ
 """
+
+
 # avec P le point P et p le point R ou Q
 def vect(P, p):
     res = []
@@ -27,15 +48,17 @@ def vect(P, p):
         res.append(p[i] - P[i])
     return res
 
+
 # calcul le produit de deux vecteur
 def prodVect(u, v):
     res = [0, 0, 0]
-    res[0] = u[1]*v[2] - u[2]*v[1]
-    res[1] = u[2]*v[0] - u[0]*v[2]
-    res[2] = u[0]*v[1] - u[1]*v[0]
+    res[0] = u[1] * v[2] - u[2] * v[1]
+    res[1] = u[2] * v[0] - u[0] * v[2]
+    res[2] = u[0] * v[1] - u[1] * v[0]
     return res
 
-tab = prodVect([3,5,2],[8,1,3])
+
+tab = prodVect([3, 5, 2], [8, 1, 3])
 print(tab)
 
 
@@ -60,6 +83,7 @@ def is_edge(v1, v2):
             return True
     return False
 
+
 def all_valid_pairs(obj, t):
     # compare each pair of vertices and chek if they are valid
     for v1 in obj.vertices:
@@ -67,10 +91,10 @@ def all_valid_pairs(obj, t):
             if v1 != v2 and is_edge(v1, v2) or np.linalg.norm(np.subtract(v1, v2)) < t:
                 valid_pairs.append((v1, v2))
 
+
 # all_valid_pairs(obj, t)
 # print("valid pairs")
 # print(valid_pairs)
-
 
 
 ####################################
@@ -79,14 +103,14 @@ def all_valid_pairs(obj, t):
 # remove the vertex v1 from the mesh
 def remove_vertex(obj, vertex_index):
     # Remove the vertex from the vertex list
-    obj.vertices = np.delete(obj.vertices, vertex_index, 0) 
+    obj.vertices = np.delete(obj.vertices, vertex_index, 0)
 
 
 # supprime une face
 # remove_vertex(obj, 0)
-    
-ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_faces() )
-#ps.show()
+
+ps_mesh = ps.register_surface_mesh("spot", obj.only_coordinates(), obj.only_faces())
+# ps.show()
 
 L = obj.ordered_boundary()
 print(L)
